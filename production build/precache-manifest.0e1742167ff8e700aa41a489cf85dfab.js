22699374caa0f06afb420c0aa0f4f787f71bc5fc
TheBoo19 <fearntn@yandex.ru>
2020-03-12 21:50:13 +0300

    created production build

---
+++ production build/precache-manifest.0e1742167ff8e700aa41a489cf85dfab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ee0c9d7884130fa46b12454f926bad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93ec11117b891208df67",
+    "url": "/static/css/main.08977825.chunk.css"
+  },
+  {
+    "revision": "a5a6c9e8674b3b2268a6",
+    "url": "/static/js/2.813c611d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.813c611d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ec11117b891208df67",
+    "url": "/static/js/main.55a965b6.chunk.js"
+  },
+  {
+    "revision": "c27f76a36f8d51c29fd4",
+    "url": "/static/js/runtime-main.7bc453c4.js"
+  }
+]);
